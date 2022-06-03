7f0ff0728777360ad122fa7dc0e8f30ce6744a05
deekshitha22 <deekshithab1999@gmail.com>
2022-06-02 19:52:24 -0500

    worked on opening modal dialog in upload covid test report component

--- src/app/upload-covid-test-report/upload-covid-test-report.component.ts
+++ src/app/upload-covid-test-report/upload-covid-test-report.component.ts
@@ -94,6 +94,18 @@ else{
   
   upload(privacy: any) {
 
+      console.log('covid_report',this.formData);
+      this.covidreportservice.uploadfile(this.formData).subscribe((resp: any) => {
+        this.modalService.open(privacy, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+          this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+        console.log('response',resp);
+        console.log('transactionid',resp.transaction_id);
+        localStorage.setItem('transactionid',resp.transaction_id);
+        location.reload();
+      })
     }
 
     logout(){
